45101d68cae374ef779df99e291c5f46d0e71245
Carlos Leite <carlosleite@criticalmanufacturing.com>
2024-03-03 13:30:27 +0000

    End of section "Creating initial Solution and Projects". Add fastendpoints Refactor BooksEndpoints

---
+++ src/RiverBooks.Books/BookService.cs
@@ -0,0 +1,13 @@
+﻿namespace RiverBooks.Books;
+
+internal class BookService : IBookService
+{
+    public List<BookDto> ListBooks()
+    {
+        return [ 
+            new BookDto(Guid.NewGuid(), "Harry potter and the philosopher stone", "J.K. Rowling"),
+            new BookDto(Guid.NewGuid(), "Harry potter and the secret chamber", "J.K. Rowling"),
+            new BookDto(Guid.NewGuid(), "Harry potter and the prisioner of askaban", "J.K. Rowling")
+        ];
+    }
+}
---
+++ src/RiverBooks.Books/ListBooksEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+
+namespace RiverBooks.Books;
+
+// REPR design pattern - Request Endpoint Response
+internal class ListBooksEndpoint(IBookService bookService) : EndpointWithoutRequest<ListBooksResponse>
+{
+    private readonly IBookService _bookService = bookService;
+
+    public override void Configure()
+    {
+        Get("/books");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken cancellationToken = default)
+    {
+        var books = _bookService.ListBooks();
+
+        await SendAsync(new ListBooksResponse()
+        {
+            Books = books
+        });
+    }
+}
