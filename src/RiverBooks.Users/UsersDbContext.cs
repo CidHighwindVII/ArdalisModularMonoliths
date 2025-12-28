46fd170aae46cc6bad32da4b22b96546116368ac
Carlos Leite <carlosleite93@hotmail.com>
2025-12-28 11:57:59 +0000

    End of create user modules

---
+++ src/RiverBooks.Users/UsersDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RiverBooks.Users;
+
+public class UsersDbContext : IdentityDbContext
+{
+    public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) { }
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("Users");
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<decimal>().HavePrecision(18, 6);
+    }
+}
---
+++ src/RiverBooks.Users/UsersModuleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace RiverBooks.Users;
+
+public static class UsersModuleExtensions
+{
+    public static IServiceCollection AddUserModuleServices(this IServiceCollection services, ConfigurationManager config, 
+        ILogger logger)
+    {
+        string? connectionString = config.GetConnectionString("Users");
+        services.AddDbContext<UsersDbContext>(config => config.UseSqlServer(connectionString));
+        services.AddIdentityCore<ApplicationUser>().AddEntityFrameworkStores<UsersDbContext>();
+
+        logger.Information("{Module} module services registered", "Users");
+
+        return services;
+    }
+}
